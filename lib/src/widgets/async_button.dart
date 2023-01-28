fa7f68bdc09b5edf45186f6463380e18df4b9f48
Vlad Hrybok <vgribok@hotmail.com>
2023-01-28 14:03:44 -0500

    Async buttons now require timeout to prevent indefinite UI freezes

--- lib/src/widgets/async_button.dart
+++ lib/src/widgets/async_button.dart
@@ -4,34 +4,42 @@ class ElevatedAsyncButton extends AsyncButton {
   ElevatedAsyncButton(
       StateProvider<bool>  asyncActionProgressProvider,
       {
-        required super.child, required super.onPressed, required super.onDisplayError, required super.onLogError, super.key
-      }
-      ) : super(asyncActionProgressProvider,
-          (context, ref, innerChild, internalKey, internalOnPress) =>
-          ElevatedButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
-  );
+        required super.child,
+        required super.onPressed, required super.onDisplayError, required super.onLogError,
+        super.timeout = const Duration(seconds: 5),
+        super.key
+      }) : super(asyncActionProgressProvider,
+              (context, ref, innerChild, internalKey, internalOnPress) =>
+                ElevatedButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
+           );
 }
 
 class OutlinedAsyncButton extends AsyncButton {
   OutlinedAsyncButton(
       StateProvider<bool>  asyncActionProgressProvider,
       {
-        required super.child, required super.onPressed, required super.onDisplayError, required super.onLogError, super.key
+        required super.child,
+        required super.onPressed, required super.onDisplayError, required super.onLogError,
+        super.timeout = const Duration(seconds: 5),
+        super.key
       }) : super(asyncActionProgressProvider,
-          (context, ref, innerChild, internalKey, internalOnPress) =>
-          OutlinedButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
-  );
+            (context, ref, innerChild, internalKey, internalOnPress) =>
+                OutlinedButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
+          );
 }
 
 class TextAsyncButton extends AsyncButton {
   TextAsyncButton(
       StateProvider<bool>  asyncActionProgressProvider,
       {
-        required super.child, required super.onPressed, required super.onDisplayError, required super.onLogError, super.key
+        required super.child,
+        required super.onPressed, required super.onDisplayError, required super.onLogError,
+        super.timeout = const Duration(seconds: 5),
+        super.key
       }) : super(asyncActionProgressProvider,
-          (context, ref, innerChild, internalKey, internalOnPress) =>
-          TextButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
-  );
+              (context, ref, innerChild, internalKey, internalOnPress) =>
+                TextButton(onPressed: internalOnPress, key: internalKey, child: innerChild)
+          );
 }
 
 class FloatingActionAsyncButton extends AsyncButton {
@@ -40,19 +48,22 @@ class FloatingActionAsyncButton extends AsyncButton {
       StateProvider<bool>  asyncActionProgressProvider,
       {
         required String tooltip,
-        required super.child, required super.onPressed, required super.onDisplayError, required super.onLogError, super.key
+        required super.child,
+        required super.onPressed, required super.onDisplayError, required super.onLogError,
+        super.timeout = const Duration(seconds: 5),
+        super.key
       }) : super(
-      asyncActionProgressProvider,
-          (context, ref, innerChild, internalKey, internalOnPress) =>
-          FloatingActionButton(
-            key: internalKey,
-            onPressed: internalOnPress,
-            tooltip: tooltip,
-            foregroundColor: context.colorScheme.onPrimary,
-            backgroundColor: context.colorScheme.primary,
-            child: innerChild,
-          )
-  );
+              asyncActionProgressProvider,
+              (context, ref, innerChild, internalKey, internalOnPress) =>
+                  FloatingActionButton(
+                    key: internalKey,
+                    onPressed: internalOnPress,
+                    tooltip: tooltip,
+                    foregroundColor: context.colorScheme.onPrimary,
+                    backgroundColor: context.colorScheme.primary,
+                    child: innerChild,
+                  )
+          );
 }
 
 class IconAsyncButton extends AsyncButton {
@@ -61,18 +72,21 @@ class IconAsyncButton extends AsyncButton {
       StateProvider<bool>  asyncActionProgressProvider,
       {
         required String tooltip,
-        required Widget icon, required super.onPressed, required super.onDisplayError, required super.onLogError, super.key
+        required Widget icon,
+        required super.onPressed, required super.onDisplayError, required super.onLogError,
+        super.timeout = const Duration(seconds: 5),
+        super.key
       }) : super(
-      asyncActionProgressProvider,
-          (context, ref, innerChild, internalKey, internalOnPress) =>
-          IconButton(
-            key: internalKey,
-            onPressed: internalOnPress,
-            tooltip: tooltip,
-            icon: innerChild,
-          ),
-      child: icon
-  );
+              asyncActionProgressProvider,
+              child: icon,
+              (context, ref, innerChild, internalKey, internalOnPress) =>
+                  IconButton(
+                    key: internalKey,
+                    onPressed: internalOnPress,
+                    tooltip: tooltip,
+                    icon: innerChild,
+                  ),
+          );
 }
 
 abstract class AsyncButton extends ConsumerWidget {
@@ -83,6 +97,7 @@ abstract class AsyncButton extends ConsumerWidget {
   final String Function() _onDisplayError;
   final String Function(Object error) _onLogError;
   final Widget _child;
+  final Duration _timeout;
 
   const AsyncButton(
       this._asyncActionProgressProvider,
@@ -95,13 +110,15 @@ abstract class AsyncButton extends ConsumerWidget {
         required String Function() onDisplayError,
         /// Returns a detailed error message to be logged for debugging
         required String Function(Object error) onLogError,
+        required Duration timeout,
         super.key
       }) :
         _onPressed = onPressed,
         _onDisplayError = onDisplayError,
         _onLogError = onLogError,
-        _child = child
-  ;
+        _child = child,
+        _timeout = timeout
+      ;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -114,7 +131,7 @@ abstract class AsyncButton extends ConsumerWidget {
     );
   }
 
-  Future _internalOnPress(BuildContext context, WidgetRef ref) async {
+  Future<void> _internalOnPress(BuildContext context, WidgetRef ref) async {
 
     final bool asyncActionInProgress = ref.read(_asyncActionProgressProvider);
     if(asyncActionInProgress) {
@@ -126,7 +143,7 @@ abstract class AsyncButton extends ConsumerWidget {
 
     try {
       progressStateProvider.state = true;
-      await _onPressed();
+      await _onPressed().timeout(_timeout);
     } catch(e) {
       _onLogError(e).debugPrint();
       context.showSnackBar(_onDisplayError());
