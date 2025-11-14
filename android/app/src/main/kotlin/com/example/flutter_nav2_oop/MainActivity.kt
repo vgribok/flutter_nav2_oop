321c7bd82cf9f412c036be8238922afc2d6221a3
Vlad Hrybok <vgribok@hotmail.com>
2025-11-14 17:38:46 -0500

    Updated build tools and SDKs.

--- android/app/src/main/kotlin/com/example/flutter_nav2_oop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_nav2_oop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/android/app/src/main/kotlin/com/hrybok/flutternav/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hrybok.flutternav.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
